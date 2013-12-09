ae507546a25380c9c75d786aea29275671f24800
Darkwater124 <Darkwater124@gmail.com>
2013-12-09 21:56:08 +0100

    Added prompt to fish config

--- config.fish
+++ config.fish
@@ -18,8 +18,62 @@ function snv
 end
 
 
+set prompt_show_hostname no
+switch (hostname)
+    case dark-desktop
+        set prompt_color 00aa00
+    case dark-laptop
+        set prompt_color 2266ff
+    case novaember
+        set prompt_color ffaf00
+    case '*'
+        set prompt_color ababab
+        set prompt_show_hostname yes
+end
+
+function fish_prompt_separator
+    set_color ababab
+    echo -n ' // '
+end
+
+function fish_prompt
+    set -l last_status $status
+
+    echo -n ' '
+
+    if [ $last_status -ne 0 ]
+        set_color ff6622
+        echo -n $last_status
+
+        fish_prompt_separator
+    end
+
+    set_color $prompt_color
+    echo -n (whoami)
+
+    fish_prompt_separator
+
+    if test $prompt_show_hostname = yes
+        set_color $prompt_color
+        echo -n (hostname)
+
+        fish_prompt_separator
+    end
+
+    set_color efefef
+    echo -n (prompt_pwd)
+
+    fish_prompt_separator
+end
+
+function fish_right_prompt
+    
+end
+
+
 stty stop '' -ixon -ixoff
 
 
 set TERM xterm-256color
 set PATH ~/bin $PATH
+set GREP_COLOR '30;48;5;221'
