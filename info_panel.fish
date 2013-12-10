cc1eaa5a3f5138b1917cef747d8bf649028bbf20
Darkwater124 <Darkwater124@gmail.com>
2013-12-10 23:23:53 +0100

    Update for remote mpd server (specify host 'mpd' in /etc/hosts)

--- info_panel.fish
+++ info_panel.fish
@@ -1,9 +1,6 @@
 #!/usr/bin/fish
 
 
-set mpd_host localhost
-
-
 rm /tmp/info_panel ^&-
 touch /tmp/info_panel
 
@@ -20,34 +17,82 @@ for i in (seq (xrandr --current | grep -cE '\bconnected\b'))
 end
 
 
+function clock ; echo '#fa0' ; echo 'clock'
+    echo (date +'%a %b %d %H:%M:%S')
+end
+
+function host ; echo '#ff0' ; echo 'arch_10x10'
+    echo (hostname)
+end
+
+function load ; echo '#af0' ; echo 'cpu'
+    echo (uptime | grep -oE '[0-9.]+, [0-9.]+, [0-9.]+' | cut -d' ' -f2 | sed 's/[, ]//g')
+end
+
+function volume
+    echo '#0f4'
+    echo 'spkr_01'
+
+    set volume (amixer sget Master | grep -oE '\[1?[0-9]{1,2}%\]' | grep -oE '[0-9]+')
+
+    if [ (hostname) = 'dark-desktop' ]
+        set now_playing (mpc -h 'mpd')
+        if [ $status -eq 0 ]
+            echo -n '^ib(1)'
+            echo -n (math (echo $now_playing[3] | grep -oE '[0-9]+') - 60 | gdbar -max 40 -fg '#0fa' -bg '#054' -h 2 | \
+                sed -re's/(\^r\([0-9x]*)\)/\1+0+2)/g')
+            echo -n '^p(-80)^ib(1)'
+        end
+        echo $volume | gdbar -fg '#0f4' -bg '#052' -h 2 | sed -re's/(\^r\([0-9x]*)\)/\1+0-2)/g'
+    else
+        echo $volume | gdbar -fg '#0f4' -bg '#052' -h 2
+    end
+end
+
+function mpd
+    set now_playing (mpc -h 'mpd' -f '[%title%]|[%file%]')
+    if [ $status -ne 0 ]; return; end
+
+    echo "$now_playing[2]" | grep '\[playing\]' ^/dev/null >/dev/null
+    if [ $status -eq 0 ]
+        echo '#0fa'
+    else
+        echo '#aaa'
+    end
+    
+    set now_playing (basename -s'.mp3' $now_playing[1] | sed -re's/\(.*\)//g' -e's/^ +| +$|^.* – //g')
+
+    echo 'note'
+
+    echo -n $now_playing
+end
+
+function battery ; echo '#0ff' ; echo 'bat_low_02'
+    if ls /sys/class/power_supply/BAT/capacity ^/dev/null >/dev/null
+        cat /sys/class/power_supply/BAT/capacity | gdbar -fg '#0ff' -bg '#055' -h 2
+    end
+end
+
+
+touch /tmp/info_panel_update
+
 while true
     begin
         set items
         function add_item
+            if [ (count $argv) -ne 3 ]; return; end
             set items "^fg($argv[1]) ^i(/home/dark/.icons/sm4tik/xbm/$argv[2].xbm) $argv[3] " $items
         end
 
-        set icons
-        function add_icon
-            set icons "^fg($argv[1])^i(/home/dark/.icons/sm4tik/xbm/$argv[2].xbm) " $icons
-        end
 
 
-        set volume (amixer sget Master | grep -oE '\[1?[0-9]{1,2}%\]' | grep -oE '[0-9]+')
+        add_item (clock)
+        add_item (host)
+        add_item (load)
+        add_item (volume)
+        add_item (mpd)
+        add_item (battery)
 
-
-        add_item '#fa0' 'clock'         (date +'%a %b %d %H:%M:%S')
-        add_item '#ff0' 'arch_10x10'    (hostname)
-        add_item '#af0' 'cpu'           (uptime | grep -oE '[0-9.]+, [0-9.]+, [0-9.]+' | cut -d' ' -f2 | sed 's/[, ]//g')
-        add_item '#0f4' 'spkr_01'       (echo $volume | gdbar -fg '#0f4' -bg '#053' -h 2 -sw 6 -ss 2)
-        #add_item '#aaa' 'note'          (mpc -h $mpd_host -f '[%title%]|[%file%]' | head -n 1 | sed -re's/\(.*\)//g' \
-        #                                                                                             -e's/^ +| +$//g')
-        add_item '#0ff' 'bat_low_02'    (cat /sys/class/power_supply/BAT/capacity | gdbar -fg '#0ff' -bg '#055' -h 2)
-
-
-        for i in $icons
-            echo -n "$i"
-        end
 
         for i in $items
             echo -n "^bg()^fg(#707070)\\\\$i"
@@ -56,5 +101,7 @@ while true
         echo
     end >>/tmp/info_panel
 
-    sleep 1s
+    inotifywait -e close_write -t 1 /tmp/info_panel_update ^/dev/null >/dev/null
+
+    if [ $status -eq 1 ]; exit; end
 end
--- media.fish
+++ media.fish
@@ -5,16 +5,22 @@ set volume (amixer sget Master | grep -oE '\[1?[0-9]{1,2}%\]' | grep -oE '[0-9]+
 for i in $argv
     switch $i
         case 'toggle'
-            mpc play
+            mpc -h mpd toggle
         case 'next'
-            mpc next
+            mpc -h mpd next
         case 'prev'
-            mpc prev
+            mpc -h mpd prev
         case 'up'
             amixer sset Master (math $volume + 10)'%'
         case 'down'
             amixer sset Master (math $volume - 10)'%'
+        case 'rup'
+            mpc -h mpd volume +2
+        case 'rdown'
+            mpc -h mpd volume -2
         case 'mute'
             amixer sset Master toggle
     end
 end
+
+touch /tmp/info_panel_update
