d3ded82b8f7862d0105aaa4e2425802637c43ccc
Darkwater124 <Darkwater124@gmail.com>
2014-02-23 21:26:44 +0100

    Improved stream counter

--- checkvps.fish
+++ checkvps.fish
@@ -3,5 +3,5 @@
 set load (ssh nv "uptime | grep -oE '[0-9.]+, [0-9.]+, [0-9.]+' | cut -d',' -f2 | xargs")
 echo "$load" > /tmp/vps_load
 
-set viewers (ssh nv "netstat -nt | grep -c 81.4.121.34:1337")
+set viewers (ssh nv "netstat -nt | grep 'ESTABLISHED' | grep -c 81.4.121.34:1337")
 echo "$viewers" > /tmp/stream_viewers
