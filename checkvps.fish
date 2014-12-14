6cc62de89d745f5a11a14b50882e7eaac12e5a53
Sam Lakerveld <Darkwater124@gmail.com>
2014-12-14 22:49:49 +0100

    Removed unused binaries

--- checkvps.fish
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/fish
-
-set load (ssh nv "uptime | grep -oE '[0-9.]+, [0-9.]+, [0-9.]+' | cut -d',' -f2 | xargs")
-echo "$load" > /tmp/vps_load
-
-set viewers (ssh nv "netstat -nt | grep 'ESTABLISHED' | grep -c 81.4.121.34:1337")
-echo "$viewers" > /tmp/stream_viewers
--- dmenu-bind.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-exe=`dmenu_run -i -f  -p 'start'                            \
-               -fn 'Droid Sans Mono-10Droid Sans Mono-10'   \
-               -b -q -t -w 810                              \
-               -class 'dmenu'                               \
-               -h 24                                        \
-               -nb '#1d1f21' -nf '#ababab'                  \
-               -sb '#111111' -sf '#ffaf00'`                && eval "exec $exe"
