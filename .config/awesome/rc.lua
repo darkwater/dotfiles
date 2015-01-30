bf2f1243b674d89f51c8ff46ef94b39c3462a9d9
Sam Lakerveld <Darkwater124@gmail.com>
2015-01-30 21:03:40 +0100

    Changed default terminal to urxvt

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -71,7 +71,7 @@ naughty.config.defaults.hover_timeout    = nil
 beautiful.init("/home/dark/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "sakura"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
