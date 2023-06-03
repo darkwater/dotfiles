603e4804110b570788d0fc0563a46d6ca6141b4c
Sam Lakerveld <dark@dark.red>
2023-06-03 16:03:50 +0200

    auto: added .config/nvim/lua/config/colors.lua

---
+++ .config/nvim/lua/config/colors.lua
@@ -0,0 +1,12 @@
+require("ayu").setup {
+    mirage = true,
+    overrides = {
+        Normal = { bg = "NONE" },
+        NormalFloat = { bg = "#333844" },
+    },
+}
+
+vim.cmd[[
+colorscheme ayu
+hi link StorageClass Keyword
+]]
