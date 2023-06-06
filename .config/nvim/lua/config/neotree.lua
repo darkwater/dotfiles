51b2cb5ec3fce4dafb064d50d293a1a6713990b4
Sam Lakerveld <dark@dark.red>
2023-06-06 12:40:15 +0200

    auto: modified +48 -5 in .config/nvim/lua/config/neotree.lua

--- .config/nvim/lua/config/neotree.lua
+++ .config/nvim/lua/config/neotree.lua
@@ -1,7 +1,50 @@
+vim.g.neo_tree_remove_legacy_commands = true
+
 require("neo-tree").setup {
-  window = {
-    mappings = {
-      ["<leader>wt"] = "close_window",
-    }
-  }
+    sources = {
+        "filesystem",
+        "git_status",
+        "document_symbols",
+    },
+    window = {
+        mappings = {
+            ["<leader>wt"] = "close_window",
+        }
+    },
+    default_component_configs = {
+        indent = {
+            indent_marker = "├",
+        },
+        icon = {
+            default = "",
+            folder_empty = "󰜌",
+            folder_empty_open = "󰜌",
+        },
+        git_status = {
+            symbols = {
+                renamed   = "󰁕",
+                unstaged  = "󰄱",
+            },
+        },
+    },
+    document_symbols = {
+        kinds = {
+            File = { icon = "󰈙", hl = "Tag" },
+            Namespace = { icon = "󰌗", hl = "Include" },
+            Package = { icon = "󰏖", hl = "Label" },
+            Class = { icon = "󰌗", hl = "Include" },
+            Property = { icon = "󰆧", hl = "@property" },
+            Enum = { icon = "󰒻", hl = "@number" },
+            Function = { icon = "󰊕", hl = "Function" },
+            String = { icon = "󰀬", hl = "String" },
+            Number = { icon = "󰎠", hl = "Number" },
+            Array = { icon = "󰅪", hl = "Type" },
+            Object = { icon = "󰅩", hl = "Type" },
+            Key = { icon = "󰌋", hl = "" },
+            Struct = { icon = "󰌗", hl = "Type" },
+            Operator = { icon = "󰆕", hl = "Operator" },
+            TypeParameter = { icon = "󰊄", hl = "Type" },
+            StaticMethod = { icon = '󰠄 ', hl = 'Function' },
+        }
+    },
 }
