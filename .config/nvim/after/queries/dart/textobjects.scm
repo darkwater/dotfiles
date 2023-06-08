0d2afef62043c8a5ba17a96b97b94cdcae10dce4
Sam Lakerveld <dark@dark.red>
2023-06-08 09:44:42 +0200

    auto: added .config/nvim/after/

---
+++ .config/nvim/after/queries/dart/textobjects.scm
@@ -0,0 +1,20 @@
+; statement
+[
+  (break_statement)
+  (do_statement)
+  (expression_statement)
+  (for_statement)
+  (if_statement)
+  (return_statement)
+  (switch_statement)
+  (while_statement)
+  (assert_statement)
+  ;(labeled_statement)
+  (yield_statement)
+  (yield_each_statement)
+  (continue_statement)
+  (try_statement)
+
+  (import_or_export)
+  (local_variable_declaration)
+] @statement.outer
