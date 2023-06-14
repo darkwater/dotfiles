742c8af6bf27daaa2d0039587b3ab6e7b55fa287
Sam Lakerveld <dark@dark.red>
2023-06-14 13:44:41 +0200

    auto: added .config/nvim/lua/config/completion.lua

---
+++ .config/nvim/lua/config/completion.lua
@@ -0,0 +1,46 @@
+local cmp = require("cmp")
+
+cmp.setup {
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "buffer" },
+        { name = "path" },
+        { name = "nvim_lua" },
+    }),
+    snippet = {
+        expand = function(args)
+            require('luasnip').lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-p>"] = cmp.mapping.select_prev_item(),
+        ["<C-n>"] = cmp.mapping.select_next_item(),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<CR>"] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        }),
+        ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+    }),
+    formatting = {
+        format = function(entry, vim_item)
+            if vim.tbl_contains({ 'path' }, entry.source.name) then
+                local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
+                if icon then
+                    vim_item.kind = icon
+                    vim_item.kind_hl_group = hl_group
+                    return vim_item
+                end
+            end
+
+            return require('lspkind').cmp_format{ with_text = false }(entry, vim_item)
+        end
+    },
+}
+
+require("lspconfig")["rust_analyzer"].setup {
+    capabilities = require("cmp_nvim_lsp").default_capabilities(),
+}
