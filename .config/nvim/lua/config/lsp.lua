e598f924f49f5ab96ec4302e4747342c5a32d846
Sam Lakerveld <dark@dark.red>
2023-06-28 17:31:32 +0200

    auto: modified +21 -0 in .config/nvim/lua/config/lsp.lua

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -12,6 +12,9 @@ lsp.rust_analyzer.setup {
             cargo = {
                 loadOutDirsFromCheck = true,
             },
+            checkOnSave = {
+                command = "clippy",
+            },
             check = {
                 command = "clippy",
             },
@@ -20,9 +23,27 @@ lsp.rust_analyzer.setup {
                     enable = false,
                 },
             },
+            linkedProjects = {},
             procMacro = {
                 enable = true,
             },
         },
     },
+    on_init = function(client)
+        local path = client.workspace_folders[1].name
+        local config = client.config.settings["rust-analyzer"]
+
+        if path == "/Users/dark/gitea/comforest/firmware" then
+            config.linkedProjects = {
+                "Cargo.toml",
+                "cross/sensor/Cargo.toml",
+                "cross/hub/Cargo.toml",
+                "cross/powerbox/Cargo.toml",
+                "cross/bootloader/Cargo.toml",
+            }
+        end
+
+        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+        return true
+    end,
 }
