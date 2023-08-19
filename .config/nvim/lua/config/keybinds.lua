acd1198214180ae3160b7f2913772b77df79d959
Sam Lakerveld <dark@dark.red>
2023-08-19 20:18:32 +0200

    auto: modified +120 -15 in .config/nvim/lua/config/keybinds.lua

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -12,9 +12,84 @@ function Cmd(cmd)
     return "<Cmd>" .. cmd .. "<CR>"
 end
 
+function run_cmd(title, cmd, max_lines)
+    if max_lines == nil then
+        max_lines = 1
+    end
+
+    return function()
+        local lines = {}
+        while #lines < max_lines - 1 do
+            table.insert(lines, "")
+        end
+        table.insert(lines, "$ " .. cmd)
+
+        local notification = require("notify")(lines, vim.log.levels.INFO, {
+            title = title,
+            timeout = 60000,
+            max_lines = max_lines,
+        })
+
+        -- local task = require("overseer").new_task {
+        --     cmd = cmd,
+        -- }
+        -- task:start()
+
+        -- require("notify")("foo", vim.log.levels.INFO, {
+        --     replace = notification.id,
+        -- })
+        local job_id = vim.fn.jobstart(cmd, {
+            on_stdout = function(j, data, event)
+                for _, line in ipairs(data) do
+                    if line ~= "" then
+                        table.insert(lines, line)
+                        while #lines > max_lines do
+                            table.remove(lines, 1)
+                        end
+                        notification = require("notify")(lines, vim.log.levels.INFO, {
+                            replace = notification.id,
+                        })
+                    end
+                end
+            end,
+            on_stderr = function(j, data, event)
+                for _, line in ipairs(data) do
+                    if line ~= "" then
+                        table.insert(lines, line)
+                        while #lines > max_lines do
+                            table.remove(lines, 1)
+                        end
+                        notification = require("notify")(lines, vim.log.levels.ERROR, {
+                            replace = notification.id,
+                        })
+                    end
+                end
+            end,
+            on_exit = function(j, code, event)
+                if code == 0 then
+                    body = table.concat(lines, "\n")
+                    notification = require("notify")(body, vim.log.levels.INFO, {
+                        title = "Done",
+                        replace = notification.id,
+                        timeout = 2000,
+                    })
+                else
+                    notification = require("notify")("Error!", vim.log.levels.ERROR, {
+                        replace = notification.id,
+                        timeout = 5000,
+                    })
+                end
+            end,
+        })
+    end
+end
+
 local nvimdir = vim.fn.stdpath "config"
 
 local telescope = require("telescope.builtin")
+local hop = require("hop")
+local flutter = require("flutter")
+
 function with_input(prompt, name, fn)
     return function()
         vim.ui.input(prompt, function(input)
@@ -29,11 +104,20 @@ function with_opts(fn, opts)
     end
 end
 
+function hover()
+    if vim.fn.expand('%:t') == 'Cargo.toml' and require('crates').popup_available() then
+        require('crates').show_popup()
+    else
+        vim.lsp.buf.hover()
+    end
+end
+
 local keymap = {}
 
-keymap.K = { vim.lsp.buf.hover, "Hover" }
+keymap.K = { hover, "Hover" }
 keymap["<C-k>"] = { vim.lsp.buf.signature_help, "Signature help" }
 vim.keymap.set("i", "<C-k>", "<Cmd>lua vim.lsp.buf.signature_help()<CR>", { noremap = true })
+vim.keymap.set("i", "<C-S-k>", "<C-k>", { noremap = true })
 
 keymap.d = {}
 keymap.d.s = {}
@@ -46,11 +130,16 @@ keymap.g.t = { telescope.lsp_type_definitions, "Go to type definition" }
 
 keymap["<leader>"] = { name = "+leader" }
 
-keymap["\\"] = { "<leader><leader>", "+language-specific", noremap = false }
-keymap["<leader>"]["<leader>"] = { name = "+language-specific" }
-keymap["<leader>"]["<leader>"].f = { name = "+flutter" }
-keymap["<leader>"]["<leader>"].f.a = { name = "+android" }
-keymap["<leader>"]["<leader>"].f.a.c = {
+keymap["<M-Bslash>"] = { "<Nop>", "No-op" }
+keymap["<Bslash>"] = { function()
+    require("flutter").toggle_flutter_terminal("flutter run")
+end, "Run Executor" }
+
+-- keymap["<Bslash>"] = { "<leader><leader>", "+language-specific", noremap = false }
+-- keymap["<leader>"] = { name = "+language-specific" }
+keymap["<leader>"].F = { name = "+flutter" }
+keymap["<leader>"].F.a = { name = "+android" }
+keymap["<leader>"].F.a.c = {
     function()
         vim.ui.input(
             "Enter IP address: ",
@@ -61,11 +150,25 @@ keymap["<leader>"]["<leader>"].f.a.c = {
     end,
     "Connect over TCP",
 }
-keymap["<leader>"]["<leader>"].f.f = { Cmd("FlutterRun"), "Run" }
-keymap["<leader>"]["<leader>"].f.r = { Cmd("FlutterRestart"), "Restart" }
-keymap["<leader>"]["<leader>"].f.h = { Cmd("FlutterReload"), "Hot reload" }
-keymap["<leader>"]["<leader>"].f.d = { Cmd("FlutterDevices"), "Devices" }
-keymap["<leader>"]["<leader>"].f.s = { Cmd("FlutterQuit"), "Stop" }
+keymap["<leader>"].F.f = { with_opts(flutter.toggle_flutter_terminal, "flutter run"), "Run" }
+keymap["<leader>"].F.r = { flutter.send_to_flutter("r"), "Hot reload" }
+keymap["<leader>"].F.R = { flutter.send_to_flutter("R"), "Hot restart" }
+keymap["<leader>"].F.q = { flutter.send_to_flutter("q"), "Stop" }
+keymap["<leader>"].F.b = { run_cmd("Build runner", "dart run build_runner build", 5), "Build runner" }
+
+keymap["<leader>"].c = { name = "+cargo" }
+keymap["<leader>"].c.a = {
+    function()
+        vim.ui.input(
+            "Cargo add:",
+            function(args)
+                vim.cmd("!cargo add " .. args)
+            end
+        )
+    end,
+    "Add dependency",
+}
+keymap["<leader>"].c.r = { with_opts(flutter.toggle_flutter_terminal, "cargo run"), "Run" }
 
 keymap["<leader>"].f = { name = "+file" }
 keymap["<leader>"].f.s = { Cmd("source %"), "Source file" }
@@ -85,17 +188,17 @@ keymap["<leader>"].g.p = { Cmd("Gitsigns preview_hunk_inline"), "Preview hunk" }
 keymap["<leader>"].g.s = { Cmd("Telescope git_status"), "Status" }
 keymap["<leader>"].g.S = { Cmd("Telescope git_stash"), "Stash" }
 
-local diag_opts = { severity_limit = "warn", disable_coordinates = true }
+local diag_opts = { severity_limit = "error", disable_coordinates = true }
 keymap["<leader>"].l = { name = "+lsp" }
 keymap["<leader>"].l.a = { vim.lsp.buf.code_action, "Code actions" }
 keymap["<leader>"].l.c = { telescope.lsp_incoming_calls, "Incoming calls" }
 keymap["<leader>"].l.C = { telescope.lsp_outgoing_calls, "Outgoing calls" }
-keymap["<leader>"].l.e = { with_opts(telescope.diagnostics, diag_opts), "Errors/warnings" }
+keymap["<leader>"].l.e = { with_opts(telescope.diagnostics, diag_opts), "Errors" }
 keymap["<leader>"].l.d = { telescope.diagnostics, "All diagnostics" }
 keymap["<leader>"].l.i = { telescope.lsp_implementations, "Implementations" }
 keymap["<leader>"].l.r = { telescope.lsp_references, "References" }
 keymap["<leader>"].l.s = { telescope.lsp_document_symbols, "Symbols (document)" }
-keymap["<leader>"].l.S = { telescope.lsp_workspace_symbols, "Symbols (workspace)" }
+keymap["<leader>"].l.S = { with_input("Workspace symbol search:", "query", telescope.lsp_workspace_symbols), "Symbols (workspace)" }
 
 keymap["<leader>"].p = { name = "+project/plugins" }
 keymap["<leader>"].p.i = { Cmd("PlugInstall"), "Install plugins" }
@@ -150,7 +253,9 @@ keymap["]"].h = { Cmd("Gitsigns next_hunk"), "Next hunk" }
 keymap["]"].q = { Cmd("cnext"), "Next quickfix" }
 keymap["]"].l = { Cmd("lnext"), "Next location" }
 
-keymap["<S-space>"] = { Cmd("b#"), "Last buffer" }
+keymap["'"] = { hop.hint_words, "Last buffer" }
+
+keymap["<Enter>"] = { Cmd("b#"), "Last buffer" }
 
 keymap["<C-'>"] = {
     function()
