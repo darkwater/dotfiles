e3331724379bb12d857fee3f6b9301e7e837b5ea
Sam Lakerveld <dark@dark.red>
2023-08-26 16:17:23 +0200

    auto: modified +1 -1 in .config/nvim/lua/config/autoformat.lua

--- .config/nvim/lua/config/autoformat.lua
+++ .config/nvim/lua/config/autoformat.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = {"*.rs", "*.dart"},
+    pattern = {"*.rs", "*.dart", "*.ts", "*.js"},
     callback = function (ev)
         vim.lsp.buf.format()
     end,
