4f3a40e2dea5540a545e877a75f46fc37958c747
Sam Lakerveld <dark@dark.red>
2023-10-24 20:48:01 +0200

    auto: modified +25 -0 in .config/nvim/lua/config/rust.lua

--- .config/nvim/lua/config/rust.lua
+++ .config/nvim/lua/config/rust.lua
@@ -56,4 +56,29 @@ vim.g.rustaceanvim = {
             },
         },
     },
+    -- on_init = function(client)
+    --     local config = client.config.settings["rust-analyzer"]
+
+    --     for k,folder in pairs(client.workspace_folders) do
+    --         local path = folder.name
+
+    --         if path == "/Users/dark/gitea/comforest/firmware" then
+    --             config.linkedProjects = {
+    --                 "Cargo.toml",
+    --                 "cross/sensor/Cargo.toml",
+    --                 "cross/hub/Cargo.toml",
+    --                 "cross/powerbox/Cargo.toml",
+    --                 "cross/bootloader/Cargo.toml",
+    --             }
+    --             config.procMacro = {
+    --                 enable = true,
+    --             }
+
+    --             client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    --             return true
+    --         end
+    --     end
+
+    --     return true
+    -- end,
 }
