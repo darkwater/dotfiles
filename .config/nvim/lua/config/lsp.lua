42af2262a953493588ad9a7229e194a0568ad1dd
Sam Lakerveld <dark@dark.red>
2023-12-01 23:47:16 +0100

    auto: modified +4 -4 in .config/nvim/lua/config/lsp.lua

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -3,10 +3,10 @@ local lsp = require("lspconfig")
 
 local null_ls = require('null-ls')
 null_ls.setup {
-    sources = {
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.formatting.prettier,
-    },
+    -- sources = {
+    --     null_ls.builtins.diagnostics.eslint,
+    --     null_ls.builtins.formatting.prettier,
+    -- },
 }
 
 -- require("lsp-progress").setup {}
