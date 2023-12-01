874d7dd88b2c204ea406c9b48952ad9cfebf1004
Sam Lakerveld <dark@dark.red>
2023-12-01 23:47:08 +0100

    auto: added .config/nvim/lua/config/ufo.lua

---
+++ .config/nvim/lua/config/ufo.lua
@@ -0,0 +1,9 @@
+vim.o.foldcolumn = "0"
+vim.o.foldlevel = 99
+vim.o.foldlevelstart = 99
+
+-- require("ufo").setup {
+--     provider_selector = function(bufnr, filetype, buftype)
+--         return {"treesitter"}
+--     end
+-- }
