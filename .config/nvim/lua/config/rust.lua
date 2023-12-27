e65bbdc0aeba9b1b876b6c44d9727c7748580591
Sam Lakerveld <dark@dark.red>
2023-12-27 20:57:46 +0100

    auto: modified +93 -58 in .config/nvim/lua/config/rust.lua

--- .config/nvim/lua/config/rust.lua
+++ .config/nvim/lua/config/rust.lua
@@ -10,71 +10,106 @@ require('crates').setup {
     },
 }
 
-local function get_rust_analyzer_path()
-    local handle = io.popen("rustup which rust-analyzer")
-    local result = handle:read("*a")
-    handle:close()
-    return result:match("^(.-)%s*$") -- Trim the result
-end
+vim.g.rustaceanvim = function()
+    local function get_rust_analyzer_path()
+        local handle = io.popen("rustup which rust-analyzer")
+        local result = handle:read("*a")
+        handle:close()
+        return result:match("^(.-)%s*$") -- Trim the result
+    end
 
-local rust_analyzer_path = get_rust_analyzer_path()
+    local rust_analyzer_path = get_rust_analyzer_path()
 
-if rust_analyzer_path == "" then
-    print("rust-analyzer not found")
-    return
-end
+    if rust_analyzer_path == "" then
+        print("rust-analyzer not found")
+        return
+    end
 
-vim.g.rustaceanvim = {
-    tools = {
-        executor = require("rustaceanvim.executors.toggleterm"),
-        inlay_hints = {
+    -- Update this path
+    local extension_path = vim.env.HOME .. '/.vscode/extensions/vadimcn.vscode-lldb-1.10.0/'
+    local codelldb_path = extension_path .. 'adapter/codelldb'
+    local liblldb_path = extension_path .. 'lldb/lib/liblldb'
+
+    -- The liblldb extension is .so for Linux and .dylib for MacOS
+    liblldb_path = liblldb_path .. ".dylib"
+
+    local cfg = require('rustaceanvim.config')
+    return {
+        tools = {
+            executor = require("rustaceanvim.executors.toggleterm"),
+            inlay_hints = {
+            },
         },
-    },
-    server = {
-        cmd = { rust_analyzer_path },
-        capabilities = require("cmp_nvim_lsp").default_capabilities(),
-        settings = function (path)
-            local linkedProjects
-            if path == "/Users/dark/gitea/comforest/firmware"
-            or path == "/Users/dark/gitea/comforest/firmware/cross" then
-                linkedProjects = {
-                    "/Users/dark/gitea/comforest/firmware/Cargo.toml",
-                    "/Users/dark/gitea/comforest/firmware/cross/Cargo.toml",
-                }
-            end
+        server = {
+            cmd = { rust_analyzer_path },
+            capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            settings = function (path)
+                local linkedProjects
+                if path == "/Users/dark/gitea/comforest/firmware"
+                    or path == "/Users/dark/gitea/comforest/firmware/cross" then
+                    linkedProjects = {
+                        "/Users/dark/gitea/comforest/firmware/Cargo.toml",
+                        "/Users/dark/gitea/comforest/firmware/cross/Cargo.toml",
+                    }
+                end
 
-            return {
-                ["rust-analyzer"] = {
-                    assist = {
-                        importMergeBehavior = "last",
-                        importPrefix = "by_self",
-                    },
-                    cargo = {
-                        -- loadOutDirsFromCheck = true,
-                    },
-                    checkOnSave = {
-                        command = "clippy",
-                    },
-                    completion = {
-                        postfix = {
-                            enable = false,
+                return {
+                    ["rust-analyzer"] = {
+                        assist = {
+                            importMergeBehavior = "last",
+                            importPrefix = "by_self",
                         },
-                    },
-                    diagnostics = {
-                        enable = true,
-                        disabled = {
-                            -- "unresolved-proc-macro",
-                            "inactive-code",
+                        cargo = {
+                            -- loadOutDirsFromCheck = true,
                         },
+                        checkOnSave = {
+                            command = "clippy",
+                        },
+                        completion = {
+                            postfix = {
+                                enable = false,
+                            },
+                        },
+                        diagnostics = {
+                            enable = true,
+                            disabled = {
+                                -- "unresolved-proc-macro",
+                                "inactive-code",
+                            },
+                        },
+                        procMacro = {
+                            enable = true,
+                        },
+                        linkedProjects = linkedProjects,
                     },
-                    procMacro = {
-                        enable = true,
-                    },
-                    linkedProjects = linkedProjects,
-                },
-            }
-        end,
-    },
+                }
+            end,
+        },
+        dap = {
+            adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+        },
+    }
+end
+
+-- vim.g.rustaceanvim = {
+    -- dap = {
+    --     adapter = {
+    --         type = "probe-rs",
+    --         port = "50000",
+    --     },
+    --     configuration = {
+    --         type = "probe-rs",
+    --         name = "Debug Firmware",
+    --         request = "launch",
+    --         program = "${workspaceFolder}/target/thumbv7em-none-eabihf/debug/powerbox",
+    --         chip = "STM32L433RC",
+    --         coreConfigs = {
+    --             {
+    --                 programBinary = "${workspaceFolder}/target/thumbv7em-none-eabihf/debug/powerbox",
+    --             },
+    --         },
+    --     },
+    -- },
     -- on_init = function(client)
     --     local config = client.config.settings["rust-analyzer"]
 
@@ -100,4 +135,4 @@ vim.g.rustaceanvim = {
 
     --     return true
     -- end,
-}
+-- }
