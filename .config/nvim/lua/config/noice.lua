2ff9ed79fed247d117f2e32f20e62c7883ffc2b0
Sam Lakerveld <dark@dark.red>
2024-02-18 22:33:56 +0100

    auto: modified +6 -6 in .config/nvim/lua/config/noice.lua

--- .config/nvim/lua/config/noice.lua
+++ .config/nvim/lua/config/noice.lua
@@ -1,7 +1,7 @@
 require("noice").setup {
-    cmdline = {
-        view = "cmdline",
-    },
+    -- cmdline = {
+    --     view = "cmdline",
+    -- },
     lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
@@ -15,11 +15,11 @@ require("noice").setup {
     },
     -- you can enable a preset for easier configuration
     presets = {
-        bottom_search = true, -- use a classic bottom cmdline for search
+        -- bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = false, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
-        inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
+        inc_rename = true, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
     },
     views = {
         mini = {
