be7257f65cbd84f9f585fc1346435d2dea766137
Sam Lakerveld <dark@dark.red>
2024-02-18 22:33:56 +0100

    auto: modified +37 -0 in .config/nvim/lua/config/telescope.lua

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -42,3 +42,40 @@ require("telescope").setup {
         },
     },
 }
+
+local M = {}
+
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+local homedir = vim.fn.expand "$HOME"
+
+M.projects = function()
+     pickers.new({}, {
+        prompt_title = "Projects",
+        finder = finders.new_oneshot_job({
+            "fd", "--exact-depth", "2", "--type", "directory", ".",
+            homedir .. "/gitea", homedir .. "/github"
+        }, {
+            -- entry_maker = function(line)
+            --     return {
+            --         value = line,
+            --         display = line,
+            --         ordinal = line,
+            --     }
+            -- end,
+        }),
+        attach_mappings = function(prompt_bufnr, map)
+            actions.select_default:replace(function()
+                actions.close(prompt_bufnr)
+                local selection = action_state.get_selected_entry()
+                vim.cmd("cd " .. selection.value)
+            end)
+            return true
+        end,
+    }):find()
+end
+
+return M
