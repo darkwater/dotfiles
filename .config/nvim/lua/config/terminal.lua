b2b98fe69aa5167c050f46a56dfb6a6cc90901c1
Sam Lakerveld <dark@dark.red>
2024-04-20 01:13:56 +0200

    auto: modified +19 -4 in .config/nvim/lua/config/terminal.lua

--- .config/nvim/lua/config/terminal.lua
+++ .config/nvim/lua/config/terminal.lua
@@ -1,4 +1,19 @@
-require("toggleterm").setup {
-    direction = "vertical",
-    size = 100,
-}
+vim.system(
+    { "osqueryi", "--list", "select 1 from connected_displays where name = \"PL3466WQ\";" },
+    { text = true },
+    function(obj)
+        local ultrawide_connected = false
+
+        if obj.code == 0 and obj.stdout == "1\n1\n" then
+            ultrawide_connected = true
+        end
+
+        vim.schedule(function()
+            print("ultrawide_connected", ultrawide_connected)
+            require("toggleterm").setup {
+                direction = "vertical",
+                size = ultrawide_connected and 120 or 80,
+            }
+        end)
+    end
+)
