245adb2a82c1e6ea68aff41489736579008ee9ad
Sam Lakerveld <dark@dark.red>
2024-04-20 01:13:56 +0200

    auto: modified +12 -3 in .config/nvim/lua/config/title.lua

--- .config/nvim/lua/config/title.lua
+++ .config/nvim/lua/config/title.lua
@@ -1,4 +1,13 @@
-local cwd = vim.fn.getcwd()
-local without_home = string.gsub(cwd, os.getenv("HOME") or os.getenv("userprofile"), "~")
+local function update_title()
+    local cwd = vim.fn.getcwd()
+    local without_home = string.gsub(cwd, os.getenv("HOME") or os.getenv("userprofile"), "~")
 
-vim.opt.titlestring = "nvim " .. without_home
+    vim.opt.titlestring = "nvim " .. without_home
+end
+
+update_title()
+
+vim.api.nvim_create_autocmd("DirChanged", {
+    pattern = "*",
+    callback = update_title,
+})
