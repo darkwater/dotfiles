70209e1d44fde68fe91423ff38e3c79c40491630
Sam Lakerveld <dark@dark.red>
2024-05-03 19:40:50 +0200

    auto: modified +8 -1 in .config/nvim/lua/config/autoformat.lua

--- .config/nvim/lua/config/autoformat.lua
+++ .config/nvim/lua/config/autoformat.lua
@@ -1,6 +1,13 @@
 vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = {"*.rs", "*.dart", "*.ts", "*.js"},
     callback = function (ev)
-        vim.lsp.buf.format()
+        -- Check if formatting is supported by any attached client
+        local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+        for _, client in ipairs(clients) do
+            if client.supports_method("textDocument/formatting") then
+                vim.lsp.buf.format()
+                break
+            end
+        end
     end,
 })
