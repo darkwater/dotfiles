b0951a2ed42bdd88f1a5b5c19f74242cc868d388
darkwater <dark@dark.red>
2024-11-01 14:30:02 +0100

    auto: modified +10 -0 in .config/nvim/lua/config/treesitter.lua

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -2,6 +2,16 @@ vim.filetype.add({
   pattern = { [".*/hypr/.*%.conf"] = "hyprlang" },
 })
 
+-- https://github.com/nvim-treesitter/nvim-treesitter/issues/4945
+local parser = require("nvim-treesitter.parsers").get_parser_configs()
+parser.dart = {
+  install_info = {
+    url = "https://github.com/UserNobody14/tree-sitter-dart",
+    files = { "src/parser.c", "src/scanner.c" },
+    revision = "8aa8ab977647da2d4dcfb8c4726341bee26fbce4", -- The last commit before the snail speed
+  },
+}
+
 require("nvim-treesitter.configs").setup {
     highlight = {
         enable = true,
