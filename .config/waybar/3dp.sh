216694b9423628df285685ab08bca27ed823382d
darkwater <dark@dark.red>
2025-03-19 22:24:06 +0100

    auto: added .config/waybar/3dp.sh

---
+++ .config/waybar/3dp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+curl -s 'https://3dp.fbk.red/printer/objects/query?webhooks&virtual_sdcard&print_stats' |
+    jq -r '.result.status | "\(.virtual_sdcard.progress * 100 | round) \(.print_stats.print_duration | round) \(.print_stats.total_duration | round)"' |
+    {
+        read progress duration total
+        if [[ -n $progress ]]; then
+            progress="$progress%"
+            left=$((total - duration))
+            now=$(date +%s)
+            eta=$((now + left))
+            eta=$(date -d @$eta +%H:%M)
+
+            echo "󰹜 $progress ($eta)"
+        fi
+    }
