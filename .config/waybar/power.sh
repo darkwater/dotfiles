cf01abb6c3f50832ef0bf842d32aa816330cc34d
darkwater <dark@dark.red>
2025-05-31 19:06:09 +0200

    auto: added .config/waybar/power.sh

---
+++ .config/waybar/power.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -s 'http://localhost:9090/api/v1/query' \
+    --url-query 'query=(delta(node_power_supply_charge_ampere[30s])*120)*node_power_supply_voltage_volt' |
+    jq -r '.data.result[0].value[1] | tonumber | round'
