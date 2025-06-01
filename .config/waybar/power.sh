1ba3dcef9caefb0a491a2e6ada7189757214888e
darkwater <dark@dark.red>
2025-06-01 17:34:08 +0200

    auto: modified +6 -2 in .config/waybar/power.sh

--- .config/waybar/power.sh
+++ .config/waybar/power.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-curl -s 'http://localhost:9090/api/v1/query' \
+out="$(curl -s 'http://localhost:9090/api/v1/query' \
     --url-query 'query=(delta(node_power_supply_charge_ampere[30s])*120)*node_power_supply_voltage_volt' |
-    jq -r '.data.result[0].value[1] | tonumber | round'
+    jq -r '.data.result[0].value[1] | tonumber | round')"
+
+if [[ "$out" != "0" ]]; then
+    echo "$out"
+fi
