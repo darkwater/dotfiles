dbe5fc5b1de125ed80c95890bba3d425810a5646
darkwater <dark@dark.red>
2026-01-19 13:45:12 +0100

    auto: added .config/nvim/lua/plugins/

---
+++ .config/nvim/lua/plugins/00_misc.lua
@@ -0,0 +1,86 @@
+return {
+  "tpope/vim-abolish", --          " better search/replace
+  "tpope/vim-characterize", --     " better ga
+  "tpope/vim-commentary", --       " toggle comments with gc
+  "tpope/vim-endwise", --          " auto-end
+  "tpope/vim-fugitive", --         " git
+  "tpope/vim-repeat", --           " more support for .
+  "tpope/vim-rsi", --              " readline-style insertion
+  "tpope/vim-speeddating", --      " increment dates
+  "tpope/vim-surround", --         " surround with pairs
+  "godlygeek/tabular", --          " tabularize code
+  "AndrewRadev/splitjoin.vim", --  " split/join lines
+
+  "MunifTanjim/nui.nvim", -- ,
+  "Saecki/crates.nvim",
+  "Shatur/neovim-ayu",
+  "akinsho/flutter-tools.nvim",
+  "akinsho/toggleterm.nvim", -- , {'tag' : '*'}
+  "andythigpen/nvim-coverage",
+  "dcampos/cmp-snippy",
+  "dcampos/nvim-snippy",
+  "evanleck/vim-svelte", -- , {'branch': 'main'}
+  "folke/noice.nvim",
+  "folke/trouble.nvim",
+  "folke/which-key.nvim",
+  "github/copilot.vim",
+  "glts/vim-magnum",
+  "glts/vim-radical",
+  "hrsh7th/cmp-buffer",
+  "hrsh7th/cmp-nvim-lsp",
+  "hrsh7th/cmp-nvim-lua",
+  "hrsh7th/cmp-path",
+  "hrsh7th/nvim-cmp",
+  "j-hui/fidget.nvim", -- , { 'tag': 'legacy' }
+  "nvimtools/none-ls.nvim",
+  "jparise/vim-graphql",
+  "kevinhwang91/nvim-ufo",
+  "kevinhwang91/promise-async",
+  "lewis6991/gitsigns.nvim",
+  "mrcjkb/rustaceanvim",
+  "darkwater/ferris.nvim",
+  "lvimuser/lsp-inlayhints.nvim",
+  "neovim/nvim-lspconfig",
+  "nvchad/nvterm",
+  "nvim-lua/plenary.nvim",
+  "nvim-lualine/lualine.nvim",
+  "nvim-neo-tree/neo-tree.nvim", -- , { 'branch': 'v3.x' }
+  "nvim-orgmode/orgmode",
+  "nvim-telescope/telescope.nvim",
+  "nvim-telescope/telescope-ui-select.nvim",
+  "aznhe21/actions-preview.nvim",
+  "nvim-tree/nvim-web-devicons",
+  "onsails/lspkind.nvim",
+  "othree/html5.vim",
+  "pangloss/vim-javascript",
+  "phaazon/hop.nvim",
+  "rcarriga/nvim-notify",
+--   "stevearc/dressing.nvim", --  " optional for vim.ui.select
+--   "stevearc/overseer.nvim",
+  "tikhomirov/vim-glsl",
+  "pest-parser/pest.vim",
+  "mfussenegger/nvim-dap",
+  "nvim-neotest/nvim-nio",
+  "rcarriga/nvim-dap-ui",
+  "vimoutliner/vimoutliner",
+  "wojciech-kulik/xcodebuild.nvim",
+  "s1n7ax/nvim-window-picker",
+  "aklt/plantuml-syntax",
+  "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+  "jbyuki/venn.nvim",
+  "goolord/alpha-nvim",
+  "iamcco/markdown-preview.nvim", -- , { 'do': { -> mkdp#util#install() }, 'for': ['markdown', 'vim-plug']}
+  "dcharbon/vim-flatbuffers",
+  "DNLHC/glance.nvim",
+  "mikesmithgh/kitty-scrollback.nvim",
+  "topaxi/pipeline.nvim",
+  "nvim-neotest/neotest",
+  "xzbdmw/colorful-menu.nvim",
+--   "olimorris/codecompanion.nvim",
+  "sindrets/diffview.nvim",
+  "mechatroner/rainbow_csv",
+  "CopilotC-Nvim/CopilotChat.nvim",
+  "2KAbhishek/nerdy.nvim",
+  "norcalli/nvim-colorizer.lua",
+  "NickvanDyke/opencode.nvim",
+}
