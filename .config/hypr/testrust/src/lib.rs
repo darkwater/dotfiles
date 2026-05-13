3215589564d0a5f7e60e700702311e1d89dade86
darkwater <dark@dark.red>
2026-05-13 10:34:28 +0200

    auto: added .config/hypr/testrust/

---
+++ .config/hypr/testrust/src/lib.rs
@@ -0,0 +1,63 @@
+use mlua::prelude::*;
+
+#[mlua::lua_module]
+fn libtestrust(lua: &Lua) -> LuaResult<LuaTable> {
+    let config = config::Config::new(lua)?;
+    config.general.gaps_in(1.)?;
+    config.general.gaps_out(50.)?;
+    config.apply()?;
+
+    println!("Hello from Rust!");
+
+    lua.create_table()
+}
+
+pub mod config {
+    use mlua::prelude::*;
+
+    pub struct Config<'a> {
+        lua: &'a Lua,
+        pub general: General,
+    }
+
+    impl<'a> Config<'a> {
+        pub fn new(lua: &'a Lua) -> LuaResult<Self> {
+            let general = General::new(lua)?;
+            Ok(Self { lua, general })
+        }
+
+        pub fn apply(&self) -> LuaResult<()> {
+            let config_table = self.lua.create_table()?;
+            config_table.set("general", self.general.table.clone())?;
+
+            self.lua
+                .globals()
+                .get::<LuaTable>("hl")?
+                .get::<LuaFunction>("config")?
+                .call::<()>(config_table)?;
+
+            Ok(())
+        }
+    }
+
+    pub struct General {
+        pub table: LuaTable,
+    }
+
+    impl General {
+        pub fn new(lua: &Lua) -> LuaResult<Self> {
+            let table = lua.create_table()?;
+            Ok(Self { table })
+        }
+
+        pub fn gaps_in(&self, gaps: f32) -> LuaResult<()> {
+            self.table.set("gaps_in", gaps)?;
+            Ok(())
+        }
+
+        pub fn gaps_out(&self, gaps: f32) -> LuaResult<()> {
+            self.table.set("gaps_out", gaps)?;
+            Ok(())
+        }
+    }
+}
